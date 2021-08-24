d615433495f3c6e225979342014adb9c336508a3
Josema Cruz <Josema@secmotic.com>
2021-08-25 01:16:24 +0200

    fixed styless modal design

--- client/src/components/StepProgress/index.jsx
+++ client/src/components/StepProgress/index.jsx
@@ -5,23 +5,10 @@ import Step from '@material-ui/core/Step';
 import StepLabel from '@material-ui/core/StepLabel';
 import './styles.scss'
 import { VscChromeClose } from 'react-icons/vsc';
-import { Button, Typography } from '@material-ui/core';
+import { Button, TextField, Select } from '@material-ui/core';
 
 function getSteps() {
   return ['Select campaign settings', 'Create an ad group', 'Create an ad'];
-}
-
-function getStepContent(step) {
-  switch (step) {
-    case 0:
-      return 'Select campaign settings...';
-    case 1:
-      return 'What is an ad group anyways?';
-    case 2:
-      return 'This is the bit I really care about!';
-    default:
-      return 'Unknown step';
-  }
 }
 
 export default function StepProgress(props) {
@@ -63,19 +50,22 @@ export default function StepProgress(props) {
 				</Stepper>
 			</div>
 			<div className="stepContents">
-				<span>cosas</span>
+				{/* 
+					TODO: Crear formularios
+				<TextField id="outlined-basic" label="Outlined" variant="outlined" />
+				<Select
+          multiple
+       /> */}
 			</div>
 			<div className="stepFooter">
-				<Typography className>{getStepContent(activeStep)}</Typography>
 				<div>
-					<Button disabled={activeStep === 0} onClick={handleBack} className>
+					<Button disabled={activeStep === 0} onClick={handleBack}>
 						Back
 					</Button>
 					<Button
 						variant="contained"
 						color="primary"
 						onClick={handleNext}
-						className
 					>
 						{activeStep === steps.length - 1 ? 'Finish' : 'Next'}
 					</Button>
